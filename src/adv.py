119490dc4be1e9724f1fa475614dbde7bf1f7c64
Joshua Horrocks <joshuabhorrocks@gmail.com>
2020-06-03 17:13:27 -0600

    Finished most of MVP, just trying to finialize direction movements

--- src/adv.py
+++ src/adv.py
@@ -1,10 +1,12 @@
+import sys
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons. (Hint: Input 'w' to go North)"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
@@ -21,8 +23,8 @@ chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
 
-
 # Link rooms together
+# n_to = north, s_to = south, etc...
 
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
@@ -33,19 +35,90 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
-#
 # Main
-#
 
 # Make a new player object that is currently in the 'outside' room.
+player = Player("outside")
+
+currentRoom = player.currentRoom
+#print(f"Current Room: {currentRoom}")
+
+currentDescription = room[currentRoom].description
+#print(f"Current Description: {currentDescription}")
+
+print("Welcome to The Game. Read the instructions and have fun!\n")
+print("Instructions: Read the prompts, then pick a direction.\nWhen inputing a direction, use 'w' for North, 'd' for East, 's' for South, and 'a' for West.\nYou can type 'q' to quit the game at anytime.\n")
+
+direction = None
 
 # Write a loop that:
-#
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
+
+while direction is None:
+    while direction is None:
+        #print(f"Current Room: {player.currentRoom}")
+        print(f"\nLocation: {currentRoom}")
+        print(currentDescription)
+        direction = input("\nWhich direction would you like to go?: ")
+        direction = direction.lower()
+
+# If the user enters a cardinal direction, attempt to move to the room there. (w = north, d = east, s = south, a = west)
 # Print an error message if the movement isn't allowed.
-#
 # If the user enters "q", quit the game.
+
+    else:
+        if direction == 'w':
+            if room[currentRoom].n_to is None:
+                print("\nInvalid direction. Please try again")
+                direction = None
+            else:
+                #print(f"Updated Room: {room[currentRoom].n_to.name}")
+                #print(f"Updated Description: {room[currentRoom].n_to.description}")
+                currentDescription = room[currentRoom].n_to.description
+                currentRoom = room[currentRoom].n_to.name
+                direction = None
+
+        if direction == 'd':
+            if room[currentRoom].e_to is None:
+                print("\nInvalid direction. Please try again")
+                direction = None
+            else:
+                #print(f"Updated Room: {room[currentRoom].n_to.name}")
+                #print(f"Updated Description: {room[currentRoom].n_to.description}")
+                currentDescription = room[currentRoom].n_to.description
+                currentRoom = room[currentRoom].n_to.name
+                direction = None
+
+        if direction == 's':
+            if room[currentRoom].s_to is None:
+                print("\nInvalid direction. Please try again")
+                direction = None
+            else:
+                #print(f"Updated Room: {room[currentRoom].n_to.name}")
+                #print(f"Updated Description: {room[currentRoom].n_to.description}")
+                currentDescription = room[currentRoom].n_to.description
+                currentRoom = room[currentRoom].n_to.name
+                direction = None
+
+        if direction == 'a':
+            if room[currentRoom].w_to is None:
+                print("\nInvalid direction. Please try again")
+                direction = None
+            else:
+                #print(f"Updated Room: {room[currentRoom].n_to.name}")
+                #print(f"Updated Description: {room[currentRoom].n_to.description}")
+                currentDescription = room[currentRoom].n_to.description
+                currentRoom = room[currentRoom].n_to.name
+                direction = None
+
+        if direction == 'q':
+            print("Thanks for playing!")
+            sys.exit()
+
+#    else:
+#        print("That's not a direction. Try inputing 'w' for North, 'd' for East, 's' for South, and 'a' for West")
+#        direction = None
+
+
--- src/room.py
+++ src/room.py
@@ -1,2 +1,14 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+from player import Player
+class Room():
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+
+    def __str__(self):
+        return f"Location: {self.name}. Description: {self.description}"
